a4172e61c08c1f6df79253d740b4e1596e7b2359
NiallMcguire <mundanelarch@gmail.com>
2023-11-29 15:15:25 +0000

    update config path in bash script

--- eval_decoding_ArchieWest.sh
+++ eval_decoding_ArchieWest.sh
@@ -43,7 +43,7 @@ module load anaconda/python-3.9.7/2021.11
 #Modify the line below to run your program. This is an example
 #=========================================================
  
-python /users/gxb18167/EEG-To-Text/eval_decoding.py --checkpoint_path /users/gxb18167/Datasets/Checkpoints/train_decoding/best/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_5e-05_5e-07_unique_sent.pt --config_path /shared/nas/data/m1/wangz3/SAO_project/AAAI_submission_code/config/decoding/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_5e-05_5e-07_unique_sent.json -cuda cuda:0
+python /users/gxb18167/EEG-To-Text/eval_decoding.py --checkpoint_path /users/gxb18167/Datasets/Checkpoints/train_decoding/best/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_5e-05_5e-07_unique_sent.pt --config_path /users/gxb18167/Datasets/Checkpoints/train_decoding/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_5e-05_5e-07_unique_sent.json -cuda cuda:0
 
 # Do not change the line below
 #=========================================================
