2c230936d9ecd6fe0f9d079dbaa7bc457db1ec0f
NiallMcguire <mundanelarch@gmail.com>
2023-12-01 11:32:37 +0000

    data augmentation added

--- generate_samples.py
+++ generate_samples.py
@@ -76,51 +76,6 @@ def generate_samples(g_model, input_z, input_t):
 
     return g_output.cpu().numpy()
 
-class Generator(nn.Module):
-    def __init__(self, noise_dim, word_embedding_dim):
-        super(Generator, self).__init__()
-
-        self.noise_dim = noise_dim
-        self.word_embedding_dim = word_embedding_dim
-
-        # Define the layers of your generator
-        self.fc_noise = nn.Linear(noise_dim, 105*8)  # Increase the size for more complexity
-        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105*8)  # Increase the size for more complexity
-        self.conv1 = nn.Conv2d(2, 128, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(128)
-        self.relu = nn.LeakyReLU(0.2)
-
-        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-
-        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
-        self.tanh = nn.Tanh()
-
-    def forward(self, noise, word_embedding):
-        # Process noise
-        noise = self.fc_noise(noise)
-        noise = noise.view(noise.size(0), 1, 105,8)  # Adjust the size to match conv1
-
-        # Process word embedding
-        word_embedding = self.fc_word_embedding(word_embedding)
-        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)  # Adjust the size to match conv1
-
-        # Concatenate noise and word embedding
-        combined_input = torch.cat([noise, word_embedding], dim=1)
-
-        # Upsample and generate the output
-        z = self.conv1(combined_input)
-        z = self.bn1(z)
-        z = self.relu(z)
-
-        z = self.conv2(z)
-        z = self.bn2(z)
-        z = self.relu(z)
-
-        z = self.conv3(z)
-        z = self.tanh(z)
-
-        return z
 
 def generate_synthetic_samples(input_sample, gen_model, word_embeddings, EEG_word_level_embeddings):
     word_embedding_dim = 50
@@ -140,14 +95,14 @@ def generate_synthetic_samples(input_sample, gen_model, word_embeddings, EEG_wor
         word_embedding = word_embeddings[word]
         input_z = create_noise(1, 100, "uniform")
 
-        word_embedding_tensor = torch.tensor(word_embedding, dtype=torch.float)
+        word_embedding_tensor = torch.tensor(word_embedding, dtype=torch.float).to(gen_model.device)
         word_embedding_tensor = word_embedding_tensor.unsqueeze(0)
 
         g_output = generate_samples(gen_model, input_z, word_embedding_tensor)
         EEG_synthetic_denormalized = (g_output * np.max(np.abs(EEG_word_level_embeddings))) + np.mean(
             EEG_word_level_embeddings)
 
-        synthetic_sample = torch.tensor(EEG_synthetic_denormalized[0][0], dtype=torch.float)
+        synthetic_sample = torch.tensor(EEG_synthetic_denormalized[0][0], dtype=torch.float).to(gen_model.device)
 
         synthetic_sample = synthetic_sample.resize(840)
         synthetic_EEG_samples.append(synthetic_sample)
