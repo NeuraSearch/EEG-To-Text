8464ed51df94f9e0d9cee950f26da98439999d99
NiallMcguire <mundanelarch@gmail.com>
2023-12-02 09:31:35 +0000

    data augmentation added`

--- generate_samples.py
+++ generate_samples.py
@@ -103,7 +103,10 @@ def generate_synthetic_samples(input_sample, gen_model, word_embeddings, EEG_wor
         word_embedding_tensor = word_embedding_tensor.unsqueeze(0)
 
         g_output = generate_samples(gen_model, input_z, word_embedding_tensor)
-        print("Data type of g_output:", type(g_output))
+        print("G_output location:", g_output.device)
+        g_output = g_output.detach()
+        print("G_output location:", g_output.device)
+
 
         EEG_synthetic_denormalized = (g_output * np.max(np.abs(EEG_word_level_embeddings))) + np.mean(
             EEG_word_level_embeddings)
