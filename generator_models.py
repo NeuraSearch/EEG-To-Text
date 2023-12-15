7f68b3101d90acc77a15dfe58a63653891362359
NiallMcguire <mundanelarch@gmail.com>
2023-12-15 13:31:49 +0000

    added support for all other GAN models

--- generator_models.py
+++ generator_models.py
@@ -7,6 +7,292 @@ if torch.cuda.is_available():
 else:
     device = "cpu"
 
+class Generator_DCGAN_1(nn.Module):
+    def __init__(self, noise_dim):
+        super(Generator_DCGAN_1, self).__init__()
+
+        self.noise_dim = noise_dim
+        #self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105 * 8)
+        #self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 8)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+
+
+    def forward(self, noise):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105, 8)
+
+        # Process word embedding
+        #word_embedding = self.fc_word_embedding(word_embedding.to(device))
+        #word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)
+
+        # Concatenate noise and word embedding
+        #combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(noise)
+        z = self.bn1(z)
+        z = self.relu(z)
+        z = self.conv2(z)
+
+        return z
+
+class Generator_DCGAN_2(nn.Module):
+    def __init__(self, noise_dim):
+        super(Generator_DCGAN_2, self).__init__()
+
+        self.noise_dim = noise_dim
+        #self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105*8)  # Increase the size for more complexity
+        #self.fc_word_embedding = nn.Linear(word_embedding_dim, 105*8)  # Increase the size for more complexity
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, noise):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105,8)  # Adjust the size to match conv1
+
+        # Process word embedding
+        #word_embedding = self.fc_word_embedding(word_embedding)
+        #word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)  # Adjust the size to match conv1
+
+        # Concatenate noise and word embedding
+        #combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(noise)
+        z = self.bn1(z)
+        z = self.relu(z)
+
+        z = self.conv2(z)
+        z = self.bn2(z)
+        z = self.relu(z)
+
+        z = self.conv3(z)
+        z = self.tanh(z)
+
+        return z
+
+class Generator_DCGAN_Text_1(nn.Module):
+    def __init__(self, noise_dim, word_embedding_dim):
+        super(Generator_DCGAN_Text_1, self).__init__()
+
+        self.noise_dim = noise_dim
+        self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105 * 8)
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 8)
+        self.conv1 = nn.Conv2d(2, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+
+
+    def forward(self, noise, word_embedding):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105, 8)
+
+        # Process word embedding
+        word_embedding = self.fc_word_embedding(word_embedding.to(device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)
+
+        # Concatenate noise and word embedding
+        combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(combined_input)
+        z = self.bn1(z)
+        z = self.relu(z)
+        z = self.conv2(z)
+
+        return z
+
+class Generator_DCGAN_Text_2(nn.Module):
+    def __init__(self, noise_dim, word_embedding_dim):
+        super(Generator_DCGAN_Text_2, self).__init__()
+
+        self.noise_dim = noise_dim
+        self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105*8)  # Increase the size for more complexity
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105*8)  # Increase the size for more complexity
+        self.conv1 = nn.Conv2d(2, 128, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, noise, word_embedding):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105,8)  # Adjust the size to match conv1
+
+        # Process word embedding
+        word_embedding = self.fc_word_embedding(word_embedding.to(device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)  # Adjust the size to match conv1
+
+        # Concatenate noise and word embedding
+        combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(combined_input)
+        z = self.bn1(z)
+        z = self.relu(z)
+
+        z = self.conv2(z)
+        z = self.bn2(z)
+        z = self.relu(z)
+
+        z = self.conv3(z)
+        z = self.tanh(z)
+
+        return z
+
+class Generator_WGAN_1(nn.Module):
+    def __init__(self, noise_dim):
+        super(Generator_WGAN_1, self).__init__()
+
+        self.noise_dim = noise_dim
+        #self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105 * 8)
+        #self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 8)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+
+
+    def forward(self, noise):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105, 8)
+
+        # Process word embedding
+        #word_embedding = self.fc_word_embedding(word_embedding.to(device))
+        #word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)
+
+        # Concatenate noise and word embedding
+        #combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(noise)
+        z = self.bn1(z)
+        z = self.relu(z)
+        z = self.conv2(z)
+
+        return z
+
+class Generator_WGAN_2(nn.Module):
+    def __init__(self, noise_dim):
+        super(Generator_WGAN_2, self).__init__()
+
+        self.noise_dim = noise_dim
+        #self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105*8)  # Increase the size for more complexity
+        #self.fc_word_embedding = nn.Linear(word_embedding_dim, 105*8)  # Increase the size for more complexity
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, noise):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105,8)  # Adjust the size to match conv1
+
+        # Process word embedding
+        #word_embedding = self.fc_word_embedding(word_embedding)
+        #word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)  # Adjust the size to match conv1
+
+        # Concatenate noise and word embedding
+        #combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(noise)
+        z = self.bn1(z)
+        z = self.relu(z)
+
+        z = self.conv2(z)
+        z = self.bn2(z)
+        z = self.relu(z)
+
+        z = self.conv3(z)
+        z = self.tanh(z)
+
+        return z
+
+class Generator_WGAN_Text_1(nn.Module):
+    def __init__(self, noise_dim, word_embedding_dim):
+        super(Generator_WGAN_Text_1, self).__init__()
+
+        self.noise_dim = noise_dim
+        self.word_embedding_dim = word_embedding_dim
+
+        # Define the layers of your generator
+        self.fc_noise = nn.Linear(noise_dim, 105 * 8)
+        self.fc_word_embedding = nn.Linear(word_embedding_dim, 105 * 8)
+        self.conv1 = nn.Conv2d(2, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+
+
+    def forward(self, noise, word_embedding):
+        # Process noise
+        noise = self.fc_noise(noise)
+        noise = noise.view(noise.size(0), 1, 105, 8)
+
+        # Process word embedding
+        word_embedding = self.fc_word_embedding(word_embedding.to(device))
+        word_embedding = word_embedding.view(word_embedding.size(0), 1, 105, 8)
+
+        # Concatenate noise and word embedding
+        combined_input = torch.cat([noise, word_embedding], dim=1)
+
+        # Upsample and generate the output
+        z = self.conv1(combined_input)
+        z = self.bn1(z)
+        z = self.relu(z)
+        z = self.conv2(z)
+
+        return z
+
 
 class Generator_WGAN_Text_2(nn.Module):
     def __init__(self, noise_dim, word_embedding_dim):
@@ -59,4 +345,18 @@ def get_generator_model(Model_name, z_size, word_embedding_dim):
 
     if Model_name == 'WGAN_Text_2.0' or "WGAN_Text_3.0":
         return Generator_WGAN_Text_2(z_size, word_embedding_dim)
+    elif Model_name == 'WGAN_Text_1.0':
+        return Generator_WGAN_Text_1(z_size, word_embedding_dim)
+    elif Model_name == 'WGAN_2.0':
+        return Generator_WGAN_2(z_size)
+    elif Model_name == 'WGAN_1.0':
+        return Generator_WGAN_1(z_size)
+    elif Model_name == 'DCGAN_Text_2.0':
+        return Generator_DCGAN_Text_2(z_size, word_embedding_dim)
+    elif Model_name == 'DCGAN_Text_1.0':
+        return Generator_DCGAN_Text_1(z_size, word_embedding_dim)
+    elif Model_name == 'DCGAN_2.0':
+        return Generator_DCGAN_2(z_size)
+    elif Model_name == 'DCGAN_1.0':
+        return Generator_DCGAN_1(z_size)
 
