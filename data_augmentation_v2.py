b5485211038abf19f676cc209f167b0cd128b846
NiallMcguire <mundanelarch@gmail.com>
2024-04-03 19:52:31 +0100

    added sentence level dataprep

--- data_augmentation_v2.py
+++ data_augmentation_v2.py
@@ -227,7 +227,7 @@ class ZuCo_dataset(Dataset):
                 fr"/users/gxb18167/Datasets/Checkpoints/{generator_name}/{generator_path}",
                 map_location=device)
 
-            print(gen_model.summary())
+            print(gen_model)
             # Load the model's state_dict onto the CPU
             gen_model.load_state_dict(checkpoint['gen_model_state_dict'])
             gen_model.to(device)
