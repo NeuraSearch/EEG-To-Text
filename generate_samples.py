88763ebc0104b2757e950a4cb554cdad1a91a14f
NiallMcguire <mundanelarch@gmail.com>
2024-04-11 11:18:07 +0100

    testing contextual

--- generate_samples.py
+++ generate_samples.py
@@ -96,21 +96,22 @@ def embedding_type_generation(text_embedding_type, input_embeddings_labels, word
                 contextual_embedding = np.concatenate(
                     (word_embeddings[prior_word], word_embeddings[current_word], word_embeddings[next_word]), axis=-1)
 
-            input_z = create_noise(1, 100, "uniform").to(device)
+            if contextual_embedding != None:
+                input_z = create_noise(1, 100, "uniform").to(device)
 
-            print("Current Word: ", current_word)
-            word_embedding_tensor = torch.tensor(contextual_embedding, dtype=torch.float)
-            word_embedding_tensor = word_embedding_tensor.unsqueeze(0)
+                print("Current Word: ", current_word)
+                word_embedding_tensor = torch.tensor(contextual_embedding, dtype=torch.float)
+                word_embedding_tensor = word_embedding_tensor.unsqueeze(0)
 
-            g_output = generate_samples(generator_name, gen_model, input_z, word_embedding_tensor)
-            g_output = g_output.to('cpu')
+                g_output = generate_samples(generator_name, gen_model, input_z, word_embedding_tensor)
+                g_output = g_output.to('cpu')
 
-            EEG_synthetic_denormalized = (g_output * np.max(np.abs(EEG_word_level_embeddings))) + np.mean(
-                EEG_word_level_embeddings)
+                EEG_synthetic_denormalized = (g_output * np.max(np.abs(EEG_word_level_embeddings))) + np.mean(
+                    EEG_word_level_embeddings)
 
-            synthetic_sample = torch.tensor(EEG_synthetic_denormalized[0][0], dtype=torch.float).to(device)
-            synthetic_sample = synthetic_sample.resize(840).to(device)
-            synthetic_EEG_samples.append(synthetic_sample.to('cpu'))
+                synthetic_sample = torch.tensor(EEG_synthetic_denormalized[0][0], dtype=torch.float).to(device)
+                synthetic_sample = synthetic_sample.resize(840).to(device)
+                synthetic_EEG_samples.append(synthetic_sample.to('cpu'))
 
     elif text_embedding_type == "Sentence_Level":
         max_sentence_length = 57
